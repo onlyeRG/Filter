33092680237efefe9600d4b42e3fd0b4c0d5c4e6
onlyeRG <hindinarutoshippuden@gmail.com>
2025-12-15 20:47:48 +0530

    Refactor bot credentials and connection strings
    Updated Telegram bot credentials and MongoDB connection details.

--- config.py
+++ config.py
@@ -1,27 +1,14 @@
-# Telegram App credentials (my.telegram.org se)
 API_ID = 27353035
 API_HASH = "cf2a75861140ceb746c7796e07cbde9e"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
 
-# Bot token (BotFather se)
-BOT_TOKEN = "8140111290:AAHkbPYHFIEiOIrQlGaNRDFyuUv-qvu2Mi8"
-
-
-# Bot owner (sirf ye user commands use kar sakta hai)
 OWNER_ID = 1327021082
 
-
-# MongoDB connection (MongoDB Atlas se)
-MONGO_URL = "mongodb+srv://poulomig644_db_user:d9MMUd5PsTP5MDFf@cluster0.q5evcku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://USER:PASS@cluster.mongodb.net/"
 DB_NAME = "filterbot"
 
+AUTO_DELETE_TIME = 300  # 5 minutes
 
-# Auto delete time (seconds)
-# 300 = 5 minutes
-AUTO_DELETE_TIME = 300
-
-
-# JIN GROUPS me bot kaam karega (multiple allowed)
-# Bot admin hona chahiye
 ALLOWED_GROUPS = [
     -1001708718174
 ]
