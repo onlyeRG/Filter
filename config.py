3d68d4a27b4b05278a6e74f990cf19a116f3ded2
onlyeRG <hindinarutoshippuden@gmail.com>
2025-12-15 20:51:55 +0530

    Change MONGO_URL for database connection
    Updated MONGO_URL with new credentials.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = "8140111290:AAHjF7nJRURv64I1SPWvwKKOAn9R1olrXHo"
 
 OWNER_ID = 1327021082
 
-MONGO_URL = "mongodb+srv://USER:PASS@cluster.mongodb.net/"
+MONGO_URL = "mongodb+srv://satyadipdas24_db_user:HtkJpIxhDM3h1qKh@cluster0.o35rffc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "filterbot2"
 
 AUTO_DELETE_TIME = 300  # 5 minutes
